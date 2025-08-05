b5c856aa50be5adcd1a671ecfdd80971e4a8aec0
sawsew467 <thangtvb.dev@gmail.com>
2025-08-05 16:25:34 +0700

    init

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on port:", ENV.PORT);
 });
